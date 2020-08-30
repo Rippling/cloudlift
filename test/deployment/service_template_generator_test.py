c3f705be99d295bd5302209b87c4b06ea1b68c46
Sannithi Balaji <sannithibalaji6@gmail.com>
2020-08-30 22:19:50 +0530

    test for udp service

--- test/deployment/service_template_generator_test.py
+++ test/deployment/service_template_generator_test.py
@@ -46,6 +46,30 @@ def mocked_service_config():
     }
 
 
+def mocked_udp_service_config():
+    return {
+        "cloudlift_version": 'test-version',
+        "notifications_arn": "some",
+        "services": {
+            "FreeradiusServer": {
+                "command": None,
+                "memory_reservation": Decimal(1024),
+                "udp_interface": {
+                    "container_port": Decimal(1812),
+                    "eip_allocaltion_id1": "eipalloc-02abb9e5e123492ee",
+                    "eip_allocaltion_id2": "eipalloc-02abb9e5e123492ee",
+                    "health_check_port": Decimal(1814),
+                    "internal": False,
+                    "nlb_enabled": True,
+                    "restrict_access_to": [
+                        "0.0.0.0/0"
+                    ]
+                }
+            }
+        }
+    }
+
+
 def mocked_fargate_service_config():
     return {
         "cloudlift_version": 'test-version',
@@ -157,11 +181,77 @@ class TestServiceTemplateGenerator(TestCase):
         template_generator.env_sample_file_path = './test/templates/test_env.sample'
         generated_template = template_generator.generate_service()
 
-        template_file_path = os.path.join(os.path.dirname(__file__), '../templates/expected_fargate_service_template.yml')
+        template_file_path = os.path.join(os.path.dirname(__file__),
+                                          '../templates/expected_fargate_service_template.yml')
+        with(open(template_file_path)) as expected_template_file:
+            assert to_json(''.join(expected_template_file.readlines())) == to_json(generated_template)
+
+    @patch('cloudlift.deployment.service_template_generator.ServiceInformationFetcher')
+    @patch('cloudlift.deployment.service_template_generator.build_config')
+    @patch('cloudlift.deployment.service_template_generator.get_account_id')
+    @patch('cloudlift.deployment.template_generator.region_service')
+    def test_generate_udp_service(self, mock_region_service, mock_get_account_id, mock_build_config,
+                                  mockServiceInformationFetcher):
+        environment = 'staging'
+        application_name = 'dummy'
+        mock_service_info_inst = mockServiceInformationFetcher.return_value
+        mock_service_info_inst.get_current_version.return_value = "1.1.1"
+        mock_service_info_inst.fetch_current_desired_count.return_value = {"FreeradiusServer": 100}
+        mock_service_configuration = MagicMock(spec=ServiceConfiguration, service_name=application_name,
+                                               environment=environment)
+        mock_service_configuration.get_config.return_value = mocked_udp_service_config()
+        mock_build_config.return_value = [("PORT", "80")]
+        mock_get_account_id.return_value = "12537612"
+        mock_region_service.get_region_for_environment.return_value = "us-west-2"
+
+        template_generator = ServiceTemplateGenerator(mock_service_configuration, self._get_env_stack())
+        template_generator.env_sample_file_path = './test/templates/test_env.sample'
+        generated_template = template_generator.generate_service()
+
+        template_file_path = os.path.join(os.path.dirname(__file__), '../templates/expected_udp_service_template.yml')
+        print(generated_template)
         with(open(template_file_path)) as expected_template_file:
             assert to_json(''.join(expected_template_file.readlines())) == to_json(generated_template)
 
     @staticmethod
     def _get_env_stack():
-        return {u'StackId': 'arn:aws:cloudformation:ap-south-1:725827686899:stack/cluster-staging/65410f80-d21c-11e8-913a-503a56826a2a', u'LastUpdatedTime': datetime.datetime(2018, 11, 9, 5, 22, 30, 691000), u'Parameters': [{u'ParameterValue': 'staging-cluster-v3', u'ParameterKey': 'KeyPair'}, {u'ParameterValue': '1', u'ParameterKey': 'ClusterSize'}, {u'ParameterValue': 'arn:aws:sns:ap-south-1:725827686899:non-prod-mumbai', u'ParameterKey': 'NotificationSnsArn'}, {u'ParameterValue': 'staging', u'ParameterKey': 'Environment'}, {u'ParameterValue': 'm5.xlarge', u'ParameterKey': 'InstanceType'}, {u'ParameterValue': '50', u'ParameterKey': 'MaxSize'}], u'Tags': [], u'Outputs': [{u'Description': 'VPC in which environment is setup', u'OutputKey': 'VPC', u'OutputValue': 'vpc-00f07c5a6b6c9abdb'}, {u'Description': 'Options used with cloudlift when building this cluster', u'OutputKey': 'CloudliftOptions', u'OutputValue': '{"min_instances": 1, "key_name": "staging-cluster-v3", "max_instances": 50, "instance_type": "m5.xlarge", "cloudlift_version": "0.9.4", "env": "staging"}'}, {u'Description': 'Maximum instances in cluster', u'OutputKey': 'MaxInstances', u'OutputValue': '50'}, {u'Description': 'Security group ID for ALB', u'OutputKey': 'SecurityGroupAlb', u'OutputValue': 'sg-095dbeb511019cfd8'}, {u'Description': 'Key Pair name for accessing the instances', u'OutputKey': 'KeyName', u'OutputValue': 'staging-cluster-v3'}, {
-            u'Description': 'ID of the 1st subnet', u'OutputKey': 'PrivateSubnet1', u'OutputValue': 'subnet-09b6cd23af94861cc'}, {u'Description': 'ID of the 2nd subnet', u'OutputKey': 'PrivateSubnet2', u'OutputValue': 'subnet-0657bc2faa99ce5f7'}, {u'Description': 'Minimum instances in cluster', u'OutputKey': 'MinInstances', u'OutputValue': '1'}, {u'Description': 'ID of the 2nd subnet', u'OutputKey': 'PublicSubnet2', u'OutputValue': 'subnet-096377a44ccb73aca'}, {u'Description': 'EC2 instance type', u'OutputKey': 'InstanceType', u'OutputValue': 'm5.xlarge'}, {u'Description': 'ID of the 1st subnet', u'OutputKey': 'PublicSubnet1', u'OutputValue': 'subnet-0aeae8fe5e13a7ff7'}, {u'Description': 'The name of the stack', u'OutputKey': 'StackName', u'OutputValue': 'cluster-staging'}, {u'Description': 'The unique ID of the stack. To be supplied to circle CI environment variables to validate during deployment.', u'OutputKey': 'StackId', u'OutputValue': 'arn:aws:cloudformation:ap-south-1:725827686899:stack/cluster-staging/65410f80-d21c-11e8-913a-503a56826a2a'}], u'CreationTime': datetime.datetime(2018, 10, 17, 14, 53, 23, 469000), u'Capabilities': ['CAPABILITY_NAMED_IAM'], u'StackName': 'cluster-staging', u'NotificationARNs': [], u'StackStatus': 'UPDATE_COMPLETE', u'DisableRollback': True, u'ChangeSetId': 'arn:aws:cloudformation:ap-south-1:725827686899:changeSet/cg901a2f5dbf984b9e9807a21da1ac7d12/7588cd05-1e2d-4dd6-85ab-12b921baa814', u'RollbackConfiguration': {}}
+        return {
+            u'StackId': 'arn:aws:cloudformation:ap-south-1:725827686899:stack/cluster-staging/65410f80-d21c-11e8-913a-503a56826a2a',
+            u'LastUpdatedTime': datetime.datetime(2018, 11, 9, 5, 22, 30, 691000),
+            u'Parameters': [{u'ParameterValue': 'staging-cluster-v3', u'ParameterKey': 'KeyPair'},
+                            {u'ParameterValue': '1', u'ParameterKey': 'ClusterSize'},
+                            {u'ParameterValue': 'arn:aws:sns:ap-south-1:725827686899:non-prod-mumbai',
+                             u'ParameterKey': 'NotificationSnsArn'},
+                            {u'ParameterValue': 'staging', u'ParameterKey': 'Environment'},
+                            {u'ParameterValue': 'm5.xlarge', u'ParameterKey': 'InstanceType'},
+                            {u'ParameterValue': '50', u'ParameterKey': 'MaxSize'}], u'Tags': [], u'Outputs': [
+                {u'Description': 'VPC in which environment is setup', u'OutputKey': 'VPC',
+                 u'OutputValue': 'vpc-00f07c5a6b6c9abdb'},
+                {u'Description': 'Options used with cloudlift when building this cluster',
+                 u'OutputKey': 'CloudliftOptions',
+                 u'OutputValue': '{"min_instances": 1, "key_name": "staging-cluster-v3", "max_instances": 50, "instance_type": "m5.xlarge", "cloudlift_version": "0.9.4", "env": "staging"}'},
+                {u'Description': 'Maximum instances in cluster', u'OutputKey': 'MaxInstances', u'OutputValue': '50'},
+                {u'Description': 'Security group ID for ALB', u'OutputKey': 'SecurityGroupAlb',
+                 u'OutputValue': 'sg-095dbeb511019cfd8'},
+                {u'Description': 'Key Pair name for accessing the instances', u'OutputKey': 'KeyName',
+                 u'OutputValue': 'staging-cluster-v3'}, {
+                    u'Description': 'ID of the 1st subnet', u'OutputKey': 'PrivateSubnet1',
+                    u'OutputValue': 'subnet-09b6cd23af94861cc'},
+                {u'Description': 'ID of the 2nd subnet', u'OutputKey': 'PrivateSubnet2',
+                 u'OutputValue': 'subnet-0657bc2faa99ce5f7'},
+                {u'Description': 'Minimum instances in cluster', u'OutputKey': 'MinInstances', u'OutputValue': '1'},
+                {u'Description': 'ID of the 2nd subnet', u'OutputKey': 'PublicSubnet2',
+                 u'OutputValue': 'subnet-096377a44ccb73aca'},
+                {u'Description': 'EC2 instance type', u'OutputKey': 'InstanceType', u'OutputValue': 'm5.xlarge'},
+                {u'Description': 'ID of the 1st subnet', u'OutputKey': 'PublicSubnet1',
+                 u'OutputValue': 'subnet-0aeae8fe5e13a7ff7'},
+                {u'Description': 'The name of the stack', u'OutputKey': 'StackName', u'OutputValue': 'cluster-staging'},
+                {
+                    u'Description': 'The unique ID of the stack. To be supplied to circle CI environment variables to validate during deployment.',
+                    u'OutputKey': 'StackId',
+                    u'OutputValue': 'arn:aws:cloudformation:ap-south-1:725827686899:stack/cluster-staging/65410f80-d21c-11e8-913a-503a56826a2a'}],
+            u'CreationTime': datetime.datetime(2018, 10, 17, 14, 53, 23, 469000),
+            u'Capabilities': ['CAPABILITY_NAMED_IAM'], u'StackName': 'cluster-staging', u'NotificationARNs': [],
+            u'StackStatus': 'UPDATE_COMPLETE', u'DisableRollback': True,
+            u'ChangeSetId': 'arn:aws:cloudformation:ap-south-1:725827686899:changeSet/cg901a2f5dbf984b9e9807a21da1ac7d12/7588cd05-1e2d-4dd6-85ab-12b921baa814',
+            u'RollbackConfiguration': {}}
