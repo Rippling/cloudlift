d9d5ad09c905b84753d336eb0614f9535f1adbf1
Senthil <senthil@rippling.com>
2020-09-04 13:18:00 +0530

    handle error in dummy app to show rest of the env vars

--- test/dummy/app.rb
+++ test/dummy/app.rb
@@ -2,8 +2,12 @@ require 'sinatra'
 require 'redis'
 
 get '/' do
-  redis_client = Redis.new(host: ENV['REDIS_HOST'])
-  "This is dummy app. Label: #{ENV['LABEL']}. Redis PING: #{redis_client.ping}"
+  begin
+    redis_client = Redis.new(host: ENV['REDIS_HOST'])
+     "This is dummy app. Label: #{ENV['LABEL']}. Redis PING: #{redis_client.ping}"
+  rescue => e
+      "This is dummy app. Label: #{ENV['LABEL']}. Redis PING: ERROR WHILE CONNECTING"
+  end
 end
 
 get '/elb-check' do
