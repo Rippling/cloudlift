9608e4231c8130bf5367bc2cd09ecd370cae747f
Senthil <senthil@rippling.com>
2020-09-07 15:26:54 +0530

    cache fetching from secrets manager

--- cloudlift/config/secrets_manager.py
+++ cloudlift/config/secrets_manager.py
@@ -2,9 +2,16 @@ from cloudlift.config import get_client_for
 from cloudlift.config.logging import log
 import json
 
+_secret_manager_cache = {}
+
 
 def get_config(name_prefix, env):
-    response = get_client_for('secretsmanager', env).get_secret_value(SecretId=f"{name_prefix}-{env}")
-    log(f"Fetched config from AWS secrets manager. Version: {response['VersionId']}")
-    secret_val = json.loads(response['SecretString'])
-    return {k: f"{response['ARN']}:{k}::{response['VersionId']}" for k in secret_val}
+    secret_name = f"{name_prefix}-{env}"
+    if secret_name not in _secret_manager_cache:
+        response = get_client_for('secretsmanager', env).get_secret_value(SecretId=secret_name)
+        log(f"Fetched config from AWS secrets manager. Version: {response['VersionId']}")
+        secret_val = json.loads(response['SecretString'])
+        _secret_manager_cache[secret_name] = {k: f"{response['ARN']}:{k}::{response['VersionId']}" for k in secret_val}
+    else:
+        log(f"Reusing cached config from AWS secrets manager")
+    return _secret_manager_cache[secret_name]
