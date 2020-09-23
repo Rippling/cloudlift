0b001bb1a3dbd7b8b2f903939ae5a1c1e4f5fd0b
Senthil <senthil@rippling.com>
2020-09-23 22:02:43 +0530

    introduce configs.repo_name

--- cloudlift/deployment/configs.py
+++ cloudlift/deployment/configs.py
@@ -1,4 +1,4 @@
-import os
+from os import getcwd, path
 
 import stringcase
 
@@ -7,6 +7,16 @@ from cloudlift.config.logging import log_bold
 
 def deduce_name(name):
     if name is None:
-        name = os.path.basename(os.getcwd())
+        name = __sanitized_dirname()
         log_bold("Assuming the service name to be: " + name)
     return stringcase.spinalcase(name)
+
+
+def repo_name():
+    res = f"{__sanitized_dirname()}-repo"
+    log_bold("Repo name is: " + res)
+    return res
+
+
+def __sanitized_dirname():
+    return path.basename(getcwd()).replace('rippling-', '')
---
+++ test/deployment/configs_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from mock import patch
+
+from cloudlift.deployment.configs import deduce_name, repo_name
+
+
+class TestDeduceName(unittest.TestCase):
+    @patch('cloudlift.deployment.configs.getcwd')
+    def test_deduce_name(self, mock_getcwd):
+        mock_getcwd.return_value = '/projects/rippling/main'
+        self.assertEqual(deduce_name(None), "main")
+
+    @patch('cloudlift.deployment.configs.getcwd')
+    def test_deduce_name_replace_rippling_prefix(self, mock_getcwd):
+        mock_getcwd.return_value = '/projects/rippling/rippling-main'
+        self.assertEqual(deduce_name(None), "main")
+
+    def test_deduce_name_with_override(self):
+        self.assertEqual(deduce_name('custom-name'), "custom-name")
+
+
+class TestRepoName(unittest.TestCase):
+    @patch('cloudlift.deployment.configs.getcwd')
+    def test_repo_name(self, mock_getcwd):
+        mock_getcwd.return_value = '/projects/rippling/rippling-main'
+        self.assertEqual(repo_name(), "main-repo")
+
+
