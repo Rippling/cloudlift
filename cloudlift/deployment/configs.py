e6564b381c2c79b41ddae5b7dc0e4bc91b015369
Aswin Karthik <aswinkarthik93@gmail.com>
2020-09-29 14:25:48 +0530

    Revert to old code for deducing name

--- cloudlift/deployment/configs.py
+++ cloudlift/deployment/configs.py
@@ -1,4 +1,4 @@
-from os import getcwd, path
+import os
 
 import stringcase
 
@@ -7,10 +7,6 @@ from cloudlift.config.logging import log_bold
 
 def deduce_name(name):
     if name is None:
-        name = __sanitized_dirname()
+        name = os.path.basename(os.getcwd())
         log_bold("Assuming the service name to be: " + name)
     return stringcase.spinalcase(name)
-
-
-def __sanitized_dirname():
-    return path.basename(getcwd())
